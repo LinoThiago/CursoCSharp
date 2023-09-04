174086496eada1909647cd717c081dd0f36c65e8
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 15:45:33 -0300

    Estoque

---
+++ 04Estoque/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04Estoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Dados do produto: {produto}");
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {produto}");
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {produto}");
+        }
+    }
+}

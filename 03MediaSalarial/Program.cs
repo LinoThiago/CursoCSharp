4890e396685d08aa0964a95e8b3e69d4917b1178
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 15:12:55 -0300

    Media Salarial

---
+++ 03MediaSalarial/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _03MediaSalarial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionarioA = new Funcionario();
+            Funcionario funcionarioB = new Funcionario();
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            funcionarioA.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionarioA.Salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            funcionarioB.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionarioB.Salario = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Salário médio = {(funcionarioA.Salario + funcionarioB.Salario)/2:F2}");
+        }
+    }
+}

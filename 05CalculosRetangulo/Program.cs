30d8c11c4ad0a0c169b0fc9493803e71e4b2dc11
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 16:35:26 -0300

    Calculos retangulos

---
+++ 05CalculosRetangulo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05CalculosRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+            retangulo.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine($"ÁREA = {retangulo.Area():F2}");
+            Console.WriteLine($"PERÍMETRO = {retangulo.Perimetro():F2}");
+            Console.WriteLine($"dIAGONAL = {retangulo.Diagonal():F2}");
+        }
+    }
+}

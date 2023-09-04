e5a4549ea1505f30912695df4d9b78c1de406d0c
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 14:44:03 -0300

    Calculo área triângulo

---
+++ 01CalculoAreaTriangulo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01CalculoAreaTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo trianguloX = new Triangulo();
+            Triangulo trianguloY = new Triangulo();
+            double areaX, areaY;
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            trianguloX.LadoA = double.Parse(Console.ReadLine());
+            trianguloX.LadoB = double.Parse(Console.ReadLine());
+            trianguloX.LadoC = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            trianguloY.LadoA = double.Parse(Console.ReadLine());
+            trianguloY.LadoB = double.Parse(Console.ReadLine());
+            trianguloY.LadoC = double.Parse(Console.ReadLine());
+            areaX = trianguloX.Area();
+            areaY = trianguloY.Area();
+            Console.WriteLine($"Área do triangulo X: {areaX:F2}");
+            Console.WriteLine($"Área do triangulo Y: {areaY:F2}");
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ 01CalculoAreaTriangulo/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01CalculoAreaTriangulo
+{
+    internal class Triangulo
+    {
+        public double LadoA;
+        public double LadoB;
+        public double LadoC;
+        public double SemiPerimetro;
+        public double Area()
+        {
+            SemiPerimetro = (LadoA + LadoB + LadoC) / 2;
+            return Math.Sqrt(SemiPerimetro * (SemiPerimetro - LadoA) * (SemiPerimetro - LadoB) * (SemiPerimetro - LadoC));
+        }
+    }
+}

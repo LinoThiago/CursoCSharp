9484093dd53888fcc9bc45b6f62680dde4ae03f1
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 16:55:15 -0300

    Aumento salarial

---
+++ 06AumentoSalarial/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06AumentoSalarial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Funcionário: {funcionario}");
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            funcionario.AumentarSalario(double.Parse(Console.ReadLine()));
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {funcionario}");
+        }
+    }
+}

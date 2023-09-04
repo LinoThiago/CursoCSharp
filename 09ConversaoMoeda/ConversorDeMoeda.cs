82a0107b4e981a311dcc616bee1f4e82ea8e0b0c
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 18:01:02 -0300

    Converção moeda membros estaticos

---
+++ 09ConversaoMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09ConversaoMoeda
+{
+    internal class ConversorDeMoeda
+    {
+        static double IOF = 0.06;
+        public static double CalcularConversao(double cotacao, double quantidade)
+        {
+            return quantidade * cotacao  * (1 + IOF);
+        }
+    }
+}
---
+++ 09ConversaoMoeda/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _09ConversaoMoeda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantidade = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Valor a ser pago em reais = {ConversorDeMoeda.CalcularConversao(cotacao,quantidade):F2}");
+        }
+    }
+}

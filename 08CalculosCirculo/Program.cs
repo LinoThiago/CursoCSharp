76c424d64a09419c7be3f4c38858a1c18f00a256
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 17:40:33 -0300

    Circulo - membros estaticos

---
+++ 08CalculosCirculo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _08CalculosCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Circunferência: {Calculadora.Circunferencia(raio)}");
+            Console.WriteLine($"Volume: {Calculadora.Volume(raio)}");
+            Console.WriteLine($"Valor de PI: {Calculadora.Pi}");
+        }
+    }
+}

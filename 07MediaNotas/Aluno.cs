9957a360b83c43c757e94b1875324150f8ffea22
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-04 17:14:09 -0300

    Soma Notas

---
+++ 07MediaNotas/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07SomaNotas
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+        public string Resultado()
+        {
+            double notaFinal = NotaFinal();
+            if(NotaFinal() > 60)
+            {
+                return $"NOTA FINAL = {notaFinal:F2}\nAPROVADO";
+            }
+            else
+            {
+                return $"NOTA FINAL = {notaFinal:F2}\nREPROVADO\nFALTARAM {60 - notaFinal:F2} PONTOS";
+            }
+        }
+    }
+}
---
+++ 07MediaNotas/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _07SomaNotas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+            Console.WriteLine(aluno.Resultado());
+        }
+    }
+}

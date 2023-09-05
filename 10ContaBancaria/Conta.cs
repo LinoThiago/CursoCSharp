16b63b3d1622f18b9f88b5429d096eb3085b7307
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-05 14:23:02 -0300

    Conta bancaria

---
+++ 10ContaBancaria/Conta.cs
@@ -0,0 +1,37 @@
+﻿namespace _10ContaBancaria
+{
+    internal class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string? Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Conta() { }
+
+        public Conta(int numeroConta, string? titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+
+        public Conta(int numeroConta, string? titular, double valor) : this(numeroConta, titular)
+        {
+            Deposito(valor);
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 5;
+        }
+
+        public override string? ToString()
+        {
+            return $"\nDados da conta:\nConta: {NumeroConta}\nTitular: {Titular}\nSaldo: {Saldo}\n";
+        }
+    }
+}
---
+++ 10ContaBancaria/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _10ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+            int numero;
+            string titular;
+
+            Console.Write("Entre com o número da conta: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            if (char.Parse(Console.ReadLine()) == 's')
+            {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                conta = new Conta(numero, titular, double.Parse(Console.ReadLine()));
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+            }
+            
+            Console.WriteLine(conta.ToString());
+
+            Console.Write("Entre com o valor de depósito: ");
+            conta.Deposito(double.Parse(Console.ReadLine()));
+            Console.WriteLine(conta.ToString());
+
+            Console.Write("Entre com o valor de saque: ");
+            conta.Saque(double.Parse(Console.ReadLine()));
+            Console.WriteLine(conta.ToString());
+
+        }
+    }
+}

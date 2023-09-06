526504d3d4f7fbb1bb9019529b429435e57e7de9
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-06 15:24:11 -0300

    reserva quarto vetor

---
+++ 12ReservaQuartoVetor/Locatario.cs
@@ -0,0 +1,13 @@
+﻿namespace _12ReservaQuartoVetor
+{
+    internal class Locatario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Email}";
+        }
+    }
+}
---
+++ 12ReservaQuartoVetor/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _12ReservaQuartoVetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int reservas;
+            Locatario[] locatarios = new Locatario[10];
+            string nome;
+            string email;
+
+            Console.Write("Quantas reservas serão realizadas: ");
+            reservas = int.Parse(Console.ReadLine());
+            for (int i = 0; i < reservas; i++)
+            {
+                Console.WriteLine($"\nReserva {i + 1}");
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                locatarios[int.Parse(Console.ReadLine()) - 1] = new Locatario { Nome = nome, Email = email};
+            }
+            Console.WriteLine("\nQuartos reservados:");
+            for(int i = 0;i < locatarios.Length; i++)
+            {
+                if(locatarios[i] != null)
+                {
+                    Console.WriteLine($"{i + 1}: {locatarios[i]}");
+                }
+            }
+        }
+    }
+}

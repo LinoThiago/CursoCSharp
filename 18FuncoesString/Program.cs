5ae844f16fb6fc9cd3557af48f8f31a15c95628e
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-11 18:12:33 -0300

    formatação date time

---
+++ 18FuncoesString/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _18FuncoesString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "     String Teste     ";
+            Console.WriteLine($"String original: {original}");
+            Console.WriteLine($"Transformando em maiusculas ->{original.ToUpper()}<-");
+            Console.WriteLine($"Transformando em minusculas ->{original.ToLower()}<-");
+            Console.WriteLine($"Removendo espaços em branco ->{original.Trim()}<-");
+            Console.WriteLine($"Index da primera ocorrencia 'T' ->{original.IndexOf('T')}<-");
+            Console.WriteLine($"Index da ultima ocorrencia 'e' ->{original.LastIndexOf('e')}<-");
+            Console.WriteLine($"Recortando do indice em diante ->{original.Substring(12)}<-");
+            Console.WriteLine($"Recortando o indice em diante e escolhendo quantidade de caracteres ->{original.Substring(12,5)}<-");
+            Console.WriteLine($"Substituindo texto ->{original.Replace("Teste","Replace")}<-");
+            Console.WriteLine($"Testando se string é nula ou vazia ->{String.IsNullOrEmpty(original)}<-");
+            Console.WriteLine($"Testando se string é nula ou contém só espaços em branco ->{String.IsNullOrWhiteSpace(original)}<-");
+            string[] strings = original.Trim().Split(' ');
+            foreach (string s in strings)
+            {
+                Console.WriteLine($"Recortando string pelo caracter ' ' ->{s}<-");
+            }
+        }
+    }
+}
---
+++ 19DateTime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace _19DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Construtor data: {new DateTime(2023, 09, 11)}");
+            Console.WriteLine($"Construtor data e hora: {new DateTime(2023, 09, 11, 17, 11, 50)}");
+            Console.WriteLine();
+            Console.WriteLine($"Builder Now: {DateTime.Now}");
+            Console.WriteLine($"Builder Today: {DateTime.Today}");
+            Console.WriteLine($"Builder UtcNow: {DateTime.UtcNow}");
+            Console.WriteLine();
+            Console.WriteLine($"Data Parse: {DateTime.Parse("2023-09-11")}");
+            Console.WriteLine($"Data Hora Parse: {DateTime.Parse("11/09/2023 17:27:43")}");
+            Console.WriteLine();
+            Console.WriteLine($"Data ParseExact: {DateTime.ParseExact("2023-09-11", "yyyy-MM-dd",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Data Hora ParseExact: {DateTime.ParseExact("11/09/2023 17:20:05", "dd/MM/yyyy HH:mm:ss",CultureInfo.InvariantCulture)}");
+            Console.WriteLine("\nPropriedades: ");
+            Console.WriteLine($"Data: {DateTime.Now.Date}");
+            Console.WriteLine($"Dia: {DateTime.Now.Day}");
+            Console.WriteLine($"Dia da semana: {DateTime.Now.DayOfWeek}");
+            Console.WriteLine($"Dia do ano: {DateTime.Now.DayOfYear}");
+            Console.WriteLine($"Hora: {DateTime.Now.Hour}");
+            Console.WriteLine($"Tipo de data: {DateTime.Now.Kind}");
+            Console.WriteLine($"Milisegundos: {DateTime.Now.Millisecond}");
+            Console.WriteLine($"Minuto: {DateTime.Now.Minute}");
+            Console.WriteLine($"Mês: {DateTime.Now.Month}");
+            Console.WriteLine($"Segundos: {DateTime.Now.Second}");
+            Console.WriteLine($"Ticks: {DateTime.Now.Ticks}");
+            Console.WriteLine($"Hora completa: {DateTime.Now.TimeOfDay}");
+            Console.WriteLine($"Ano: {DateTime.Now.Year}");
+            Console.WriteLine();
+            Console.WriteLine("\nFormatação: ");
+            Console.WriteLine($"Data por extenso: {DateTime.Now.ToLongDateString()}");
+            Console.WriteLine($"Data: {DateTime.Now.ToShortDateString()}");
+            Console.WriteLine($"Hora: {DateTime.Now.ToLongTimeString()}");
+            Console.WriteLine($"Hora minuto: {DateTime.Now.ToShortTimeString()}");
+            Console.WriteLine($"Formatada: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+            Console.WriteLine($"Adicionar 2 horas: {DateTime.Now.AddHours(2)}");
+            Console.WriteLine($"Diferença de data: {new DateTime(2023, 12, 25).Subtract(DateTime.Now).Days}");
+
+        }
+    }
+}

1979618a85a7317d6ddf498d6bb660733d28c6dc
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-11 16:24:08 -0300

    Condição ternaria

---
+++ 17CondicionalTernaria/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _17CondicionalTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            string maioridade = (idade < 18) ? "Menor de Idade" : "Maior de idade";
+            Console.WriteLine(maioridade);
+        }
+    }
+}

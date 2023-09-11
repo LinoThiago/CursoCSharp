4e33a29047cbc410d931019c34472f78daf1b0b2
ThiagoLino311 <thiagolinocsm@hotmail.com>
2023-09-11 18:33:47 -0300

    formatação e operações

--- 20TimeSpan/Program.cs
+++ 20TimeSpan/Program.cs
@@ -17,6 +17,24 @@
             Console.WriteLine($"From segundos: {TimeSpan.FromSeconds(1.5)}");
             Console.WriteLine($"From milisegundos: {TimeSpan.FromMilliseconds(1.5)}");
             Console.WriteLine($"From ticks: {TimeSpan.FromTicks(900000000L)}");
+            Console.WriteLine();
+            Console.WriteLine($"Dias: {new TimeSpan(5, 15, 21, 30, 500).Days}");
+            Console.WriteLine($"Horas: {new TimeSpan(5, 15, 21, 30, 500).Hours}");
+            Console.WriteLine($"Minutos: {new TimeSpan(5, 15, 21, 30, 500).Minutes}");
+            Console.WriteLine($"Segundos: {new TimeSpan(5, 15, 21, 30, 500).Seconds}");
+            Console.WriteLine($"Milisegundos: {new TimeSpan(5, 15, 21, 30, 500).Milliseconds}");
+            Console.WriteLine($"Ticks: {new TimeSpan(5, 15, 21, 30, 500).Ticks}");
+            Console.WriteLine();
+            Console.WriteLine($"Dias: {new TimeSpan(5, 15, 21, 30, 500).TotalDays}");
+            Console.WriteLine($"Horas: {new TimeSpan(5, 15, 21, 30, 500).TotalHours}");
+            Console.WriteLine($"Minutos: {new TimeSpan(5, 15, 21, 30, 500).TotalMinutes}");
+            Console.WriteLine($"Segundos: {new TimeSpan(5, 15, 21, 30, 500).TotalSeconds}");
+            Console.WriteLine($"Milisegundos: {new TimeSpan(5, 15, 21, 30, 500).TotalMilliseconds}");
+            Console.WriteLine();
+            Console.WriteLine($"Soma 1h + 1h = {new TimeSpan(1,0,0).Add(new TimeSpan(1,0,0))}");
+            Console.WriteLine($"Subtração 1h + 5m = {new TimeSpan(1,0,0).Subtract(new TimeSpan(0,5,0))}");
+            Console.WriteLine($"Multiplicação 1h * 5 = {new TimeSpan(1,0,0).Multiply(5)}");
+            Console.WriteLine($"Divisão 1h / 5 = {new TimeSpan(1,0,0).Divide(5)}");
         }
     }
 }
